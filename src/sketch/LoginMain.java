b758d76c05b366c9eec6ea63335fa76403f24013
Klaudia <klaudias@stud.ntnu.no>
2015-02-24 11:45:51 +0100

    Added calendar

---
+++ src/sketch/LoginMain.java
@@ -0,0 +1,26 @@
+package sketch;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+
+public class LoginMain extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = FXMLLoader.load(LoginController.class.getResource("Loginv3.fxml"));
+			Scene scene = new Scene(root,1280,700);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
