f6fe5f520ce0a1cb878797b6b28ae12827d4e2fe
marenpg <maren.gulnes@gmail.com>
2015-03-02 13:47:14 +0100

    Query

---
+++ src/application/Query.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.sql.*;
+
+
+/**
+* This class only contains static methods for getting information from the database
+*
+* @author Gruppe9
+*
+*/
+public class Query {
+	
+
+}
