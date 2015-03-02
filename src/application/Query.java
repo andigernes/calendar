ab4e1f51feedf80cb2e6ca765952634a15bcd718
marenpg <maren.gulnes@gmail.com>
2015-03-02 14:01:22 +0100

    Query cont.

--- src/application/Query.java
+++ src/application/Query.java
@@ -2,14 +2,39 @@ package application;
 
 import java.sql.*;
 
+import db.DBConnection;
+
 
 /**
-* This class only contains static methods for getting information from the database
-*
-* @author Gruppe9
-*
-*/
+ * This class only contains static methods for getting information from the database
+ *
+ * @author Gruppe9
+ *
+ */
 public class Query {
-	
+
+	/**
+	 * 
+	 * @param username
+	 * @return true id user exists, false otherwise
+	 */
+
+	public boolean userExists(String username){
+		String check = "SELECT * FROM User WHERE Username ='"+username+"'";
+		Statement st;
+		ResultSet rs;
+		try{
+			st = DBConnection).createStatement();
+			rs = st.executeQuery(queryCheck);
+			if (rs.next()) {
+				st.close();
+				return true;
+			}
+		}catch (SQLException e){
+
+		}
+
+		return true;
+	}
 
 }
