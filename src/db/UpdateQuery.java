84808621fbeec92f76fbd3bb0915d1eca893a7d4
andigernes <andigernes@gmail.com>
2015-03-04 13:22:49 +0100

    small change

--- src/db/UpdateQuery.java
+++ src/db/UpdateQuery.java
@@ -12,11 +12,9 @@ public class UpdateQuery {
 	 * save event in the database
 	 */
 	
-	//Metoden gjør at man kan legge inn data i databasen via GUI-et.
-	public static boolean saveInDb(Appointment ap){																	
-		//Spørring mot databasen som setter inn en tabell med data i Event tabellen.
+	public static boolean saveInDb(Appointment ap){																			//Spørring mot databasen som setter inn en tabell med data i Event tabellen.																	
 		String query = String.format("INSERT INTO Event(Start, End, Name, Description, Location, Number_of_attendants, Username, Groupname) VALUES ('%s','%s','%s','%s','%s',1, '%s', null)", 
-				ap.getStartDateTime(), ap.getEndDateTime(), ap.getName(), ap.getDescription(), ap.getLocation(), "Maren");
+				ap.getStartDateTime(), ap.getEndDateTime(), ap.getName(), ap.getDescription(), ap.getLocation(), "Maren");	//Spørring mot databasen som setter inn en tabell med data i Event tabellen.
 		int check;																											//Oppretter en variabel check.
 		try{																												//Fordi vi kan få en exception så må vi prøve om koden vil gi en exception.
 			check = db.DBConnection.getInstance().update(query);															//Setter check til å være en ny rad i tabellen i databasen og utfører innsettingen ved en oppkobling.  
