3461a5d928f358b08bcbb7eaf2adaa93fac74fbe
andigernes <andigernes@gmail.com>
2015-03-04 12:15:18 +0100

    Lagt til kommentarer til metodene i queryklassen

--- src/db/Query.java
+++ src/db/Query.java
@@ -15,23 +15,23 @@ public class Query {
 	 * Checks username and password for login
 	 */
 
-	public static boolean authenticate(String username, String password) throws SQLException{
-		String query = "SELECT * FROM User WHERE Username ='"+username+"' AND Password = '"+password+"'";
-		ResultSet rs;
-		rs = DBConnection.getInstance().query(query);
-		if(rs.next()){
-			return true;
+	public static boolean authenticate(String username, String password) throws SQLException{				//Sjekker om en bruker som logger inn faktisk eksisterer i databasen. 
+		String query = "SELECT * FROM User WHERE Username ='"+username+"' AND Password = '"+password+"'";	//Spørring mot databasen som henter brukernavn og passord fra databasen.
+		ResultSet rs;																						//Oppretter en variabel av type ResultSet.
+		rs = DBConnection.getInstance().query(query);														//Sender spørringen til databasen via en oppkobling og lagrer den i variabelen rs.
+		if(rs.next()){																						//Sjekker om en bruker eksisterer i databasen.
+			return true;																					//Om det er tilfellet at brukeren eksisterer, så returner true.
 		}
-		return false;
+		return false;																						//Om det ikke er tilfellet returner false.
 
 
 	}
 	
-	public ResultSet getDataFromEventTable(String username) throws SQLException
+	public ResultSet getDataFromEventTable(String username) throws SQLException		//Henter data fra Event tabellen i databasen.
 	{
-		DBConnection connection = DBConnection.getInstance();
-		String query = "SELECT * FROM Event WHERE username = '" + username + "'";
-		return connection.query(query);
+		DBConnection connection = DBConnection.getInstance();						//Gjør en oppkobling til databasen.		
+		String query = "SELECT * FROM Event WHERE username = '" + username + "'";	//Spørringen mot databasen som henter Eventer basert på et brukernavn.
+		return connection.query(query);												//Returnerer resultatet fra spørringen.
 	}
 
 	public static void main(String[] args) {
