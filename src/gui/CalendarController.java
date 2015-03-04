1080c7b2a40336ef9483a78bdaeb772a7c8f4df0
marenpg <maren.gulnes@gmail.com>
2015-03-04 13:04:43 +0100

    Fixing weeks in CC and C.fxml

--- src/gui/CalendarController.java
+++ src/gui/CalendarController.java
@@ -22,124 +22,135 @@ import javafx.stage.Stage;
 
 
 public class CalendarController {
-	
-    @FXML private TextField username;
-    @FXML private PasswordField password;
-    @FXML Button login;
-    @FXML Button logout;
-    @FXML Text usernamelogin;
-    private User userModel; 
-    @FXML Label usernamelabel;
-    @FXML Label passwordlabel;
-    @FXML GridPane eventArea;
-    
-    private static Calendar appointmentList = new Calendar();
-    public CalendarController() {
-    }
-   
-    public static void addAppointment(Appointment ap) {
-    	appointmentList.add(ap);
-    	db.UpdateQuery.saveInDb(ap);
-    }
-    
-    
-    @FXML
-    private void initialize() {
-    	//TODO init
 
-    }
-    
-    /**
-     * Connects with Query
-     * @return true is user name and password is valid, false otherwise
-     * @throws SQLException 
-     */
-    private boolean validUser() throws SQLException{
-    	return db.Query.authenticate(username.getText(), password.getText());
-    }
-    
-    /**
-     * Checks if username and password is valid
-     */
-    
-    private void hide(Node field){
-    	field.setDisable(true);
-    	field.setVisible(false);
-    }
-    
-    private void show(Node field){
-    	field.setDisable(false);
-    	field.setVisible(true);
-    }
-    
-    
-    @FXML
-    public void handleLogin() throws SQLException{
-    	EventRendering.representAppointment(null, null, null, eventArea);
-    	if(userModel == null) {
-	    	
-	    	if(validUser()){
-	    		userModel = new User(username.getText());
-	    		AppointmentEditController.valid(password);
-	    		AppointmentEditController.valid(username);
-	    		System.out.println("login success");
-	    		usernamelogin.setText("Logged in as: "+ username.getText());
-	    		username.clear();
-	    		password.clear();
-	    		hide(username);
-	    		hide(password);
-	    		hide(passwordlabel);
-	    		hide(usernamelabel);
-	    		login.setText("Log out");	
-	    	}else{
-	    		AppointmentEditController.invalid(password);
-	    		AppointmentEditController.invalid(username);
-	    	}
-    	} else {
-    		usernamelogin.setText("");
-    		login.setText("Log in");
-        	userModel = null;
-    		show(username);
-    		show(password);
-    		show(passwordlabel);
-    		show(usernamelabel);
-    	}
-    }
-    
-  //TODO 
-    public void setMainCalendar(MainCalendar mainCalendar){
-		//Hente ut brukers kalender
-	}	
-    
-    // AddButton
-    @FXML
-    private void handleNewAppointment() {
-    	
-    	Parent root;
-        try {
-            root = FXMLLoader.load(AppointmentEditController.class.getResource("Appointment.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("New Appointment");
-            stage.setScene(new Scene(root));
-            stage.show();
+	@FXML private TextField username;
+	@FXML private PasswordField password;
+	@FXML Button login;
+	@FXML Button logout;
+	@FXML Text usernamelogin;
+	private User userModel; 
+	@FXML Label usernamelabel;
+	@FXML Label passwordlabel;
+	@FXML GridPane eventArea;
+	@FXML Text weekIndicator;
+	@FXML GridPane weekDays;
+	int week;
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-      
+	private static Calendar appointmentList = new Calendar();
+	public CalendarController() {
+	}
+	@FXML
+	private void initialize() {
+		
+	}
+	/**
+	 * Connects with Query
+	 * @return true is user name and password is valid, false otherwise
+	 * @throws SQLException 
+	 */
+	private boolean validUser() throws SQLException{
+		return db.Query.authenticate(username.getText(), password.getText());
 	}
 
+	/**
+	 * Hides field
+	 * @param field Some field, such as user name or password 
+	 */
 
+	private void hide(Node field){
+		field.setDisable(true);
+		field.setVisible(false);
+	}
+	/**
+	 * Shows field
+	 * @param field Some field, such as user name or password 
+	 */
+	private void show(Node field){
+		field.setDisable(false);
+		field.setVisible(true);
+	}
 
+	/**
+	 * Checks if the user is valid
+	 * shows the logged in user or messages error
+	 * @throws SQLException
+	 */
+	@FXML
+	public void handleLogin() throws SQLException{
+		EventRendering.representAppointment(null, null, null, eventArea);
+		if(userModel == null) {
 
-//    public void MainCalendar(MainCalendar mainCalendar) {
-//        this.mainCalendar = mainCalendar;
-//        
-//         appointmentTable.setItems(MainCalendar.getAppointmentData());
-//
-//         Add observable info to appointment box in the calendar
-//    }
+			if(validUser()){
+				userModel = new User(username.getText());
+				AppointmentEditController.valid(password);
+				AppointmentEditController.valid(username);
+				usernamelogin.setText("Logged in as: "+ username.getText());
+				username.clear();
+				password.clear();
+				hide(username);
+				hide(password);
+				hide(passwordlabel);
+				hide(usernamelabel);
+				login.setText("Log out");	
+			}else{
+				AppointmentEditController.invalid(password);
+				AppointmentEditController.invalid(username);
+			}
+		} else {
+			usernamelogin.setText("");
+			login.setText("Log in");
+			userModel = null;
+			show(username);
+			show(password);
+			show(passwordlabel);
+			show(usernamelabel);
+		}
+	}
 	
+	/**
+	 * Fill in appointment slots for a logged in user
+	 * @param mainCalendar
+	 */
+	//TODO 
+	public void setMainCalendar(MainCalendar mainCalendar){
+		//Hente ut brukers kalender
+	}	
+	
+	/**
+	 * Add button actionEvent
+	 * Loads a new appointment
+	 */
+	@FXML
+	private void handleNewAppointment() {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(AppointmentEditController.class.getResource("Appointment.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("New Appointment");
+			stage.setScene(new Scene(root));
+			stage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	/**
+	 * Saves appointment in database
+	 * @param ap Appointment object
+	 */
+	public static void addAppointment(Appointment ap) {
+		appointmentList.add(ap);
+	}
+	/**
+	 * Delete appointment in database
+	 * @param ap Appointment object
+	 */
+	public static void deleteAppointment(Appointment ap){
+		appointmentList.remove(ap);
+	}
 	
 	
 
