87fe762c4bfff01ba70430b6a9b5aaa232c747af
Klaudia <klaudias@stud.ntnu.no>
2015-03-04 13:04:06 +0100

    events

--- src/gui/EventRendering.java
+++ src/gui/EventRendering.java
@@ -1,16 +1,12 @@
 package gui;
 
-import java.awt.Color;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
 
 import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import calendar.Appointment;
 import calendar.Calendar;
 
@@ -31,19 +27,64 @@ public class EventRendering {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param date
+	 * @param startTime
+	 * @param endTime
+	 * @param eventArea
+	 */
+	
 	public static void representAppointment(LocalDate date, String startTime, String endTime, GridPane eventArea){
 		//WeekFields weekFields = WeekFields.of(Locale.getDefault()); 
 		//int weekNumber = date.get(weekFields.weekOfWeekBasedYear());
 		
+		int starthalftime=0;
+		
+		String[] startTimeArray = startTime.split(":");
+		int startTimeStartRow = Integer.parseInt(startTimeArray[0]);
+		int startTimeEndRow = Integer.parseInt(startTimeArray[1]) ; 
+		
+		if (startTimeEndRow>0 && startTimeEndRow<30) {
+			startTimeEndRow=30;
+			starthalftime=1;
+		}
+		
+		if (startTimeEndRow>30) {
+			startTimeStartRow++;
+			startTimeEndRow=0;
+		}
+		
+		int startRow = startTimeStartRow*2 + starthalftime;
+		
+		
+		int endhalftime=0;
+
+		String[] endTimeArray = endTime.split(":");
+		int endTimeStartRow = Integer.parseInt(endTimeArray[0]);
+		int endTimeEndRow = Integer.parseInt(endTimeArray[1]) ;
+		
+		if (endTimeEndRow>0 && endTimeEndRow<30) {
+			endTimeEndRow=30;
+			endhalftime=1;
+		}
+		
+		if (endTimeEndRow>30) {
+			endTimeStartRow++;
+			endTimeEndRow=0;
+		}
+		
+		int endRow = endTimeStartRow*2 + endhalftime;
+		
 		Pane pane = new Pane();
 		pane.setStyle("-fx-background-color:#fff");
 		
-		eventArea.add(pane, 1, 20);
-		GridPane.setRowSpan(pane, 3);
+		int colspan = endRow-startRow;
+		
+		eventArea.add(pane, 1, startRow, 1, colspan);
+
+		//GridPane.setRowSpan(pane, 3);
 		GridPane.setMargin(pane, new Insets(3, 3, 3, 4));
-		
-		
-		
 		
 		
 	}
