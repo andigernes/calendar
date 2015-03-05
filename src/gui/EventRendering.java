f5ab750b11a60f4ac21cf9e3e8201496fb6b547f
marenpg <maren.gulnes@gmail.com>
2015-03-05 13:51:07 +0100

    Fixes column EventRendering

--- src/gui/EventRendering.java
+++ src/gui/EventRendering.java
@@ -42,7 +42,14 @@ public class EventRendering extends CalendarController {
 			cal.setTime(eventDate);
 			eventWeek = cal.get(Calendar.WEEK_OF_YEAR);
 			if (controller.week == eventWeek) {
-				representAppointment(appointment, controller);
+				//sets column index
+				int day = cal.get(Calendar.DAY_OF_WEEK);
+				if(day == 1){
+					day = 7;
+				}else{
+					day--;
+				}
+				representAppointment(appointment, controller, day);
 			}
 		}
 	}
@@ -59,59 +66,50 @@ public class EventRendering extends CalendarController {
 	 * 
 	 */
 
-	public static void representAppointment(Appointment appointment, CalendarController controller) {
-
-
+	public static void representAppointment(Appointment appointment,
+			CalendarController controller, int day) {
 		String name = appointment.getName();
 		String startTime = appointment.getStartTime().toString();
 		String endTime = "" + appointment.getEndTime().toString();
 		int starthalftime = 0; // 12:45
-
+		
+		// Sets grid row for startTime
 		String[] startTimeArray = startTime.split(":");
 		int startTimeStartRow = Integer.parseInt(startTimeArray[0]);
 		int startTimeEndRow = Integer.parseInt(startTimeArray[1]);
-
 		if (startTimeEndRow > 0 && startTimeEndRow < 30) {
 			starthalftime = 1;
 		}
-
 		if (startTimeEndRow > 30) {
 			startTimeStartRow++;
 		}
-
 		int startRow = startTimeStartRow * 2 + starthalftime;
-
 		int endhalftime = 0;
-
+		
+		// Sets grid row for endTime
 		String[] endTimeArray = endTime.split(":");
 		int endTimeStartRow = Integer.parseInt(endTimeArray[0]);
 		int endTimeEndRow = Integer.parseInt(endTimeArray[1]);
-
 		if (endTimeEndRow > 0 && endTimeEndRow < 30) {
 			endhalftime = 1;
 		}
-
 		if (endTimeEndRow > 30) {
 			endTimeStartRow++;
 		}
-
 		int endRow = endTimeStartRow * 2 + endhalftime;
 		int colspan = endRow - startRow;
-
+		
+		
+		
+		//Sets a pane on the grid
 		BorderPane pane = new BorderPane();
 		appointment.setNode(pane);
 
-		controller.eventArea.add(pane, 1, startRow);
+		controller.eventArea.add(pane, day, startRow);
 		GridPane.setRowSpan(pane, colspan);
-
-		// eventArea.add(pane, 1, startRow, 1, colspan);
-
 		GridPane.setMargin(pane, new Insets(2, 2, 2, 3));
-
 		Label text = new Label(name);
-
 		pane.setTop(text);
-
 		pane.setStyle("-fx-background-color:#ABE7E2");
 
 	}
