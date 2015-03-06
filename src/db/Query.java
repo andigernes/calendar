05c08975c54d7299609016cc31ea2888eb499ffb
andigernes <andigernes@gmail.com>
2015-03-06 20:09:10 +0100

    til meg selv.

--- src/db/Query.java
+++ src/db/Query.java
@@ -21,67 +21,65 @@ public class Query {
 	 * Checks whether the username and password matches the user DB
 	 */
 
-	public static boolean authenticate(String username, String password) throws SQLException {
-		String query = "SELECT * FROM User WHERE Username ='" + username + "' AND Password = '" + password + "'";
-		ResultSet rs;
-		rs = DBConnection.getInstance().query(query);
-		return rs.next(); // Returns whether a non-empty result was returned
-
+	public static boolean authenticate(String username, String password) throws SQLException {						//Metoden sjekker om brukeren eksisterer i databasen og er boolean fordi den skal svare true om brukeren eksisterer eller false hvis ikke.
+		String query = "SELECT * FROM User WHERE Username ='" + username + "' AND Password = '" + password + "'";	//Lager en spørring som ber om brukernavn og passord fra tabellen User i databasen.
+		ResultSet rs = DBConnection.getInstance().query(query);														//Henter en oppkobling til databasen og sender spørringen til den.
+		return rs.next();																							//Returnerer resultatet fra spørringen og går igjennom den.
 	}
 
 	/**
 	 * Retrieves all events owned by a specific user
 	 */
-	public static ArrayList<Appointment> getDataFromEventTable(String username) throws SQLException {
-		DBConnection connection = DBConnection.getInstance();
-		String query = "SELECT * FROM Event WHERE username = '" + username + "'";
-		ResultSet resultSet = connection.query(query);
-		ArrayList<Appointment> cal = new ArrayList<Appointment>();	
-		while(resultSet.next()){
-			Appointment a = new Appointment();
+	public static ArrayList<Appointment> getDataFromEventTable(String username) throws SQLException {	//Metoden foretar en spørring mot databasen for å hente ut alle avtaler til en bruker og lagrer dem som avtaler i en liste. 
+		DBConnection connection = DBConnection.getInstance();											//Oppretter en oppkobling til databasen.
+		String query = "SELECT * FROM Event WHERE username = '" + username + "'";						//Lager en spørring som skal hente ut alle avtaler til en bruker ut i fra brukernavnet. 
+		ResultSet resultSet = connection.query(query);													//Resultatet av spørringen er et ResultSet som må utføres metoder på for å få formatet og informasjonen vi vil ha. Bruker oppkoblingen fra andre kodelinje til å sende inn spørringen.
+		ArrayList<Appointment> cal = new ArrayList<Appointment>();										//Må opprette en ArrayList som lagrer avtale-objekter i seg og som vi skal returnere fra denne metoden. 	
+		while(resultSet.next()){																		//Benytter en while-løkke for å løpe igjennom alle radene som vi har fått fra kodelinje 4, slik at vi kan utføre metodene vi trenger for å få informasjonen vi vil ha tak i kun en gang.
+			Appointment a = new Appointment();															//Starter så å opprette nytt avtale-objekt som vi vil lagre informasjonen i, denne skal inn i listen vår fra kodelinje 3. 
 			//Add serial number
-			int sn = resultSet.getInt(1);
-			a.setSerialNumber(sn);
-			//Add start date, time and end time
-			String[] dateTime = resultSet.getString(2).split(" ");
-			String sD = dateTime[0];
-			String sT = dateTime[1];
-			dateTime =  resultSet.getString(3).split(" ");
-			String eT = dateTime[1];
-			LocalDate startDate = LocalDate.parse(sD);
-			LocalTime startTime = LocalTime.parse(sT);
-			LocalTime endTime = LocalTime.parse(eT);	
-			a.setStartDate(startDate);
-			a.setStartTime(startTime);
-			a.setEndTime(endTime);
+			int sn = resultSet.getInt(1);																//Henter ut serienummeret til en avtale fra resultatet vi fikk fra spørringen.
+			a.setSerialNumber(sn);																		//Setter så feltet SerialNumber i avtale-objektet vi opprettet i kodelinje 7 til verdien vi fikk fra kodelinje 6.
+			//Add start date, time and end time															//Vi fortsetter å plotte inn data fra databasen og inn i avatale-objektet vårt
+			String[] dateTime = resultSet.getString(2).split(" ");										//Vi må ha dato og tid til avtale-objektet vårt, på grunn av formatet DATETIME (2015-03-20 16:30:00) må vi bruke getString() for å gjøre DATETIME om til en String. Vi splitter så strengen på mellomrom (" ") for å skille dato fra tid.
+			String sD = dateTime[0];																	//Henter ut startdato fra kodelinjen over. 
+			String sT = dateTime[1];																	//Henter ut starttid for en avtale fra kodelinjen to kodelinjer over. 
+			dateTime =  resultSet.getString(3).split(" ");												//Samme begrunnelse som 3 kodelinjer over bare at nå henter vi slutdato og slutttid.
+			String eT = dateTime[1];																	//Henter ut slutttid fra listen i kodelinjen ett hakk over.
+			LocalDate startDate = LocalDate.parse(sD);													//Gjør om startdatoen som er en streng til formatet LocalDate (2007-03-06).
+			LocalTime startTime = LocalTime.parse(sT);													//Gjør om starttiden fra en streng til formatet LocalTime (10:15)
+			LocalTime endTime = LocalTime.parse(eT);													//Samme omformatering som for startdatoen.	
+			a.setStartDate(startDate);																	//Ettersom startdatoen i avtale-objektet er LocalDate, så har vi fått det formatet 3 kodelinjer over. Setter så feltet startDate til verdien fra 3 kodelinjer over.  
+			a.setStartTime(startTime);																	//Samme forklaring som over, bare at startTime er på format LocalTime. 
+			a.setEndTime(endTime);																		//Samme forklaring som rett over.
 			//Add name
-			String name = resultSet.getString(4);
-			a.setName(name);
+			String name = resultSet.getString(4);														//Lagrer navnet på avatalen fra spørringen.
+			a.setName(name);																			//Setter så feltet name i avtale-objektet til å være verdien i variabelen over. 
 			//Add description
-			String desc = resultSet.getString(5);
-			a.setDescription(desc);
+			String desc = resultSet.getString(5);														//Henter ut beskrivelsen fra resultatet fra spørringen.
+			a.setDescription(desc);																		//Setter feltet description i avtale-objektet til å være verdien i variabelen over.
 			//Add location
-			String location = resultSet.getString(6);
-			a.setLocation(location);
-			cal.add(a);
+			String location = resultSet.getString(6);													//Henter ut lokasjonen avtalen befinner seg på.
+			a.setLocation(location);																	//Setter feltet location i avtale-objektet.
+			cal.add(a);																					//Legger til avtale-objektet i listen som vi lagde.
 		}
 
-		return cal;
+		return cal;																						//Returnerer listen med alle avtalene til en bruker i.
 	}
 
-	public static ArrayList<Room> retrieveRoomsFromDB() throws SQLException {
-		DBConnection connection = DBConnection.getInstance();
-		String query = "SELECT * FROM Room";
-		ResultSet resultSet = connection.query(query);
-		ArrayList<Room> rooms = new ArrayList<>();
-		while(resultSet.next()) {
-			String nameRoom = resultSet.getString("Name_Room");
-			int capacity = resultSet.getInt("capacity");
-			Room room = new Room(nameRoom, capacity);
-			rooms.add(room);
+	public static ArrayList<Room> retrieveRoomsFromDB() throws SQLException {		//Metoden foretar en spørring mot databasen for å hente ut alle rom som er tilgjengelig og lagrer dem som rom i en liste. 
+		DBConnection connection = DBConnection.getInstance();						//Oppretter en oppkobling til databasen.
+		String query = "SELECT * FROM Room";										//Lager en spørring som skal hente ut alle rom som er tilgjengelig.
+		ResultSet resultSet = connection.query(query);								//Resultatet av spørringen er et ResultSet som må utføres metoder på for å få formatet og informasjonen vi vil ha. Bruker oppkoblingen fra andre kodelinje til å sende inn spørringen.
+		ArrayList<Room> rooms = new ArrayList<>();									//Må opprette en ArrayList som lagrer avtale-objekter i seg og som vi skal returnere fra denne metoden.
+		while(resultSet.next()) {													//Benytter en while-løkke for å løpe igjennom alle radene som vi har fått fra kodelinje 4, slik at vi kan utføre metodene vi trenger for å få informasjonen vi vil ha tak i kun en gang.
+			String nameRoom = resultSet.getString("Name_Room");						//Henter ut navnet på rommet fra resultatet ved hjelp av navnet på tabellen iformasjonen ligger i.
+			int capacity = resultSet.getInt("capacity");							//Henter ut kapasiteten til et rom på samme måte som over. 
+			Room room = new Room(nameRoom, capacity);								//Oppretter et nytt rom-objekt med et navn og en kapasitet som er fra rommene hentet fra databasen.
+			rooms.add(room);														//Legger til rommet som er laget, inn i rom-tabellen. 
 		}
 		
-		return rooms;
+		return rooms;																//Returnerer rom-tabellen med alle rommene som eksisterer.
 	}
 
 }
--- src/db/UpdateQuery.java
+++ src/db/UpdateQuery.java
@@ -16,29 +16,28 @@ public class UpdateQuery {
 	 * Saves a specific event to the database's event table
 	 */
 
-	public static boolean saveInDb(Appointment ap) {
+	public static boolean saveInDb(Appointment ap) {														//Metoden lagrer nye eventer i databasen.
 		String query = String
 				.format("INSERT INTO Event(Start, End, Name, Description, Location, Number_of_attendants, Username, Groupname) VALUES ('%s','%s','%s','%s','%s',1, '%s', null)",
 						ap.getStartDateTime(), ap.getEndDateTime(), ap.getName(), ap.getDescription(),
-						ap.getLocation(), calendar.User.getUserName());
-		int check;
+						ap.getLocation(), calendar.User.getUserName());										//Oppretter en spørring som ber databasen om å lagre en Event med forskjellig data i tabeller og gir dem verdier. 
+		int check;																							//En variabel check som skal fortelle om innsettingen var vellyket.
 		try {
-			check = db.DBConnection.getInstance().update(query);
+			check = db.DBConnection.getInstance().update(query);											//Oppretter en oppkobling til databasen og oppdaterer den med en ny rad i tabellen Event ved bruk av spørringen over.
 			// Check = number of columns affected by query
-			return check > 0;
+			return check > 0;																				//Sjekker hvor mange tabeller spørringen har oppdatert/endret.
 		} catch (SQLException e) {
 			return false;
 		}
 
 	}
 
-	public static boolean deleteInDb(Appointment ap) {
-		String deletequery = "DELETE FROM Event WHERE '"+ap.getSerialNumber().intValue()+"' = Serial_Number";
-		System.out.println(deletequery);
-		int check;
+	public static boolean deleteInDb(Appointment ap) {																//Metoden sletter en avtale fra databasen.
+		String deletequery = "DELETE FROM Event WHERE '"+ap.getSerialNumber().intValue()+"' = Serial_Number";		//En spørring som sletter en event fra databasen basert på serienummeret.
+		int check;																									//En variabel som forteller om slettingen ble utført.
 		try {
-			check = db.DBConnection.getInstance().update(deletequery);
-			return check > 0;
+			check = db.DBConnection.getInstance().update(deletequery);												//Oppretter en oppkobling til databasen som ber den slette eventen det blir bedt om.
+			return check > 0;																						//Returnerer om noe ble slettet eller ikke.
 		} catch (SQLException e) {
 			return false;
 		}
